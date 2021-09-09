a1fee2d180aa0554c907dcc24196b4a25ce6cc0f
Death Ranger <53964374+deathrangerr@users.noreply.github.com>
2021-09-09 13:49:05 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,6 @@ sudo yum update -y && yum upgrade -y
 sudo dnf upgrade -y
 sudo dnf install docker -y
 sudo service docker start && service docker status && docker ps && docker ps -a >> docker.status
-docker pull jenkins
 podman pull jenkins:2.60.3
 nohup docker run --name jenkins -p 8080:8080 -p 50000:50000 -v /root:/var/jenkins jenkins & >> jenkins.log
 
